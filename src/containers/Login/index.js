390229d3d311dd039e8506049038c0e3bb10d5de
huzhifu <2474016183@qq.com>
2018-05-20 20:01:40 +0800

    '登录页优化'

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -9,8 +9,21 @@ import styles from  './index.scss';
 export default class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { tel:'',pwd:'' };
   }
+  telHandler=(e)=>{
+    this.setState({
+      tel:e.target.value
+    });
+  };
+  pwdHandler=(e)=>{
+    this.setState({
+      pwd:e.target.value
+    });
+  };
+  loginHandler=()=>{
+    console.log('登录',this.state.tel,':::',this.state.pwd);
+  };
   render() {
     return (
       <div styleName="login">
@@ -28,13 +41,13 @@ export default class Login extends Component {
         <div styleName="content">
           <div styleName="input-wrapper">
             <span className="iconfont icon-phone"></span>
-            <input styleName="input-inner" type="number" placeholder="请输入手机号"></input>
+            <input styleName="input-inner" type="tel" placeholder="请输入手机号" value={this.state.tel} onChange={this.telHandler} maxLength="11"></input>
           </div>
           <div styleName="input-wrapper">
             <span className="iconfont icon-password"></span>
-            <input styleName="input-inner" type="password" placeholder="请输入密码"></input>
+            <input styleName="input-inner" type="password" placeholder="请输入密码" value={this.state.pwd} onChange={this.pwdHandler}></input>
           </div>
-          <button styleName="login-btn">登录</button>
+          <button styleName="login-btn" onClick={this.loginHandler}>登录</button>
           <div styleName="forget">
             <span styleName="forget__text">忘记密码？</span>
           </div>
