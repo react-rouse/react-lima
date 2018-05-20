738e973696b81fd255476d809fd4c39c9cfac958
huzhifu <2474016183@qq.com>
2018-05-20 16:11:45 +0800

    '添加登录页'

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -21,6 +21,24 @@ export default class Login extends Component {
           <div styleName="header__title">登陆</div>
           <div></div>
         </div>
+        <div styleName="logo">
+          <img styleName="logo__img" src={require('@/assets/images/login.png')} alt="登录图标">
+          </img>
+        </div>
+        <div styleName="content">
+          <div styleName="input-wrapper">
+            <span className="iconfont icon-phone"></span>
+            <input styleName="input-inner" type="number" placeholder="请输入手机号"></input>
+          </div>
+          <div styleName="input-wrapper">
+            <span className="iconfont icon-password"></span>
+            <input styleName="input-inner" type="password" placeholder="请输入密码"></input>
+          </div>
+          <button styleName="login-btn">登录</button>
+          <div styleName="forget">
+            <span styleName="forget__text">忘记密码？</span>
+          </div>
+        </div>
       </div>
     );
   }
