8150a37ebfebf4f961ef2541505dd0aacb5403c8
shenqingqing@maxrocky.com <shenqingqing@maxrocky.com>
2018-05-21 21:39:18 +0800

    忘记密码

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,7 @@ import asyncComponent from '@/utils/asyncComponent';
 
 import App from "@/containers/App";
 const Login = asyncComponent(() => import("@/containers/Login"));
-
+const Forgetpwd = asyncComponent(() => import("@/containers/Forgetpwd"));
 // react-router4 不再推荐将所有路由规则放在同一个地方集中式路由，子路由应该由父组件动态配置，组件在哪里匹配就在哪里渲染，更加灵活
 export default class RouteConfig extends Component{
     render(){
@@ -16,6 +16,7 @@ export default class RouteConfig extends Component{
                 <Switch>
                     <Route path="/" exact component={App} />
                     <Route path="/login" component={Login} />
+                    <Route path="/forgetpwd" component={Forgetpwd} />
                     <Redirect to="/" />
                 </Switch>
             </Router>
